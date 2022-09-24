5103ea4fdefc37ab3158d36dd666eb1093500903
Daisuke Sato <tiryoh@gmail.com>
2022-09-24 16:43:11 +0900

    fix: Fix save_map script

--- save_map.sh
+++ save_map.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-source $HOME/mnpp_ws/devel/setup.bash
+if [ -e ${HOME}/catkin_ws/devel/setup.bash ]; then
+    source ${HOME}/catkin_ws/devel/setup.bash
+fi
+
+MAP_SAVE_DIR=$(rospack find mini_pupper_navigation)"/maps"
+
+echo "save map to ${MAP_SAVE_DIR}"
+
 rosservice call /finish_trajectory 0
-rosservice call /write_state "filename: '${HOME}/mnpp_ws/src/minipupper_ros/mini_pupper/maps/map.pbstream'"
+rosservice call /write_state "filename: '${MAP_SAVE_DIR}/map.pbstream'"
 rosrun cartographer_ros cartographer_pbstream_to_ros_map \
-	-map_filestem=${HOME}/mnpp_ws/src/minipupper_ros/mini_pupper/maps/map \
-	-pbstream_filename=${HOME}/mnpp_ws/src/minipupper_ros/mini_pupper/maps/map.pbstream \
+	-map_filestem=${MAP_SAVE_DIR}/map \
+	-pbstream_filename=${MAP_SAVE_DIR}/map.pbstream \
 	-resolution=0.05
 
